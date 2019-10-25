88198d3dc26cd509887c8f144679b67d5feafe8b
Shashank Singla <shashank.singla@hcode.tech>
2019-10-25 12:47:01 +0000

    adding -y to few commands

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 sudo apt-get install -y python3 python3-dev python3-pip 
 sudo apt-get install -y git vim virtualenv 
 
 sudo apt-get install -y postgresql postgresql-contrib
 # need these 2 to intall psycopg2 using pip
-sudo apt-get install python-psycopg2
-sudo apt-get install libpq-dev
+sudo apt-get install -y python-psycopg2 libpq-dev
 
 
 
