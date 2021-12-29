2fb764376c1d66593e99fd100041a3578586061e
Shashank Singla <shashank.singla@hcode.tech>
2021-12-29 09:59:30 +0000

    install_server.sh edited online with Bitbucket

--- install_server.sh
+++ install_server.sh
@@ -6,8 +6,8 @@ sudo apt-get install -y python3 python3-dev python3-pip
 sudo apt-get install -y git vim virtualenv curl screen
 
 sudo apt-get install -y postgresql postgresql-contrib
-# need these 2 to intall psycopg2 using pip
-sudo apt-get install -y python-psycopg2 libpq-dev
+# need these  to intall psycopg2 using pip
+sudo apt-get install -y libpq-dev
 
 sudo pip install virtualenvwrapper
 
